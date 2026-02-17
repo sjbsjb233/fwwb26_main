a6062dc1c46f4efd86d5d55703d7f5612d8354c1
sjbsjb233 <64311244+sjbsjb233@users.noreply.github.com>
2026-02-17 14:48:10 +0800

    Initialize main.tsx with React setup

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AutoFillWebApp from './AutoFillWebApp'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AutoFillWebApp />
+  </React.StrictMode>,
+)
